9ae62dcdd1aa85d25432c39a74b3f83de8d5c002
Mohd Talha <mawtalha5112004@gmail.com>
2026-04-24 05:49:29 +0530

    db

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,35 +1,35 @@
-// import { PrismaClient } from "@prisma/client";
-
-// import "dotenv/config";
-// import { PrismaNeon } from "@prisma/adapter-neon";
-
-// const adapter = new PrismaNeon({
-//   connectionString: process.env.DATABASE_URL
-// });
-
-// console.log(adapter)
-
-// export const prisma:PrismaClient= new PrismaClient({adapter});
-
-
-
-import "dotenv/config";
-import pg from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const { Pool } = pg;
+import "dotenv/config";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
-}
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL
 });
 
-const adapter = new PrismaPg(pool);
+console.log(adapter)
 
-export const prisma = new PrismaClient({
-  adapter,
-});
+export const prisma:PrismaClient= new PrismaClient({adapter});
+
+
+
+// import "dotenv/config";
+// import pg from "pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+
+// const { Pool } = pg;
+
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL is not set");
+// }
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// const adapter = new PrismaPg(pool);
+
+// export const prisma = new PrismaClient({
+//   adapter,
+// });
